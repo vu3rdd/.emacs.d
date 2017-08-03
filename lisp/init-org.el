fb1a25131324aa645d42abd737f90415819ac6ca
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2017-08-03 20:39:21 +0530

    org: add capture template for journal and todo

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,12 +2,24 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)" "INPROGRESS(p)" "WAITING(w)" "|" "DONE" "CANCELLED")))
 
-(setq org-agenda-files '("~/src/org/"))
+(setq org-agenda-files '("~/org/"))
 
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 ;; key shortcut for org-agenda
 (global-set-key (kbd "C-c a") 'org-agenda)
+
+;; capture template for journal
+(setq org-capture-templates
+      '(
+	("j" "Journal Entry"
+         entry (file+datetree "~/org/journal.org")
+         "* %T %?"
+         :empty-lines 1)
+	("t" "TODO Entry"
+	 (file+headline "~/org/todo.org" "Tasks")
+	 "* TODO %?")
+	))
 
 (provide 'init-org)
