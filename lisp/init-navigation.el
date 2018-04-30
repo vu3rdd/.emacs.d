01c48e91f7567905c7b17fd70947d23d8be0a163
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2018-04-30 16:23:22 +0530

    navigation: disable helm

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -1,27 +1,19 @@
 (require 'init-elpa)
 
-(require-package 'helm)
-; (require-package 'counsel)
+(require-package 'ido)
+(ido-mode 1)
+
 (require-package 'projectile)
-
-(require 'helm-config)
-
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-
-(helm-mode 1)
 
 (projectile-global-mode)
 (setq projectile-completion-system 'ivy)
-;;(counsel-projectile-on)
 
 ;; Enable move point from window to window using Shift and the arrow keys
 ;; (windmove-default-keybindings)
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+;; (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (require-package 'neotree)
-
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
