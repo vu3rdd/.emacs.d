d69111d941bdb9bd201aa80c7cfd9aad049bc444
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-05-29 13:10:03 +0530

    michelson mode

--- init.el
+++ init.el
@@ -337,6 +337,11 @@
 (use-package nix-mode
   :mode "\\.nix\\'")
 
+(load "~/.emacs.d/elisp/michelson-mode.el" nil t)
+
+;; michelson mode
+(use-package deferred)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
